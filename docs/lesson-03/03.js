4442c3664e1ccf50667b67e5c7119eb33ca4a5d4
Aliaksei Redzko <aleha.redko@gmail.com>
2025-07-04 02:04:03 +0300

    third taks from third lesson JS ferst try

---
+++ docs/lesson-03/03.js
@@ -0,0 +1,13 @@
+// Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+
+function findLargest(a, b, c) {
+    switch (true) {
+        case a > b || a > c:
+            return a
+        case b > a || b > c:
+            return b
+        case c > b || c > a:
+            return c
+    }
+}
+findLargest(2, 17, 3)
