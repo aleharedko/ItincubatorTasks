267861bc9e4c8893221a1f546b7d0a5bca463f67
Aliaksei Redzko <aleha.redko@gmail.com>
2025-07-03 20:11:21 +0300

    ferst task lessons three from JS it is ferst try

---
+++ docs/lesson-03/01.js
@@ -0,0 +1,8 @@
+// Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
+
+function calculateAverage(a, b) {
+    sum = a + b;
+    return sum;
+}
+
+calculateAverage(2,2);
