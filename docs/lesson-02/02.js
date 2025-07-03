c472f9ee677557467985d4bb5b48704a2aeee731
Aliaksei Redzko <aleha.redko@gmail.com>
2025-07-03 15:20:27 +0300

    second tasks second lessons ferst try

--- docs/lesson-02/02.js
+++ docs/lesson-02/02.js
@@ -1,22 +1,31 @@
 /*
 * Напишите код, который симулирует бросок двух шестигранных игральных кубиков и определяет, выпал ли выигрышный дубль.
 * В переменные dice1 и dice2 должны присваиваться случайные целые числа от 1 до 6.
-* Дубль считается выигрышным, если на обоих дайсах выпало одно и то же число, и это число больше 3.
+* Дубль считается выигрышным, если на обоих дайсах выпало од
+но и то же число, и это число больше 3.
 * Полученное булево значение сохраните в переменной isWinningDouble
 
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
 
-/*
+let dice1 = Math.floor(Math.random() * 6) + 1;// your code
+let dice2 = Math.floor(Math.random() * 6) + 1;// your code
+
+let isWinningDouble = false // your code
+
+if (dice1 >= 1 || dice1 <= 6 && dice2 >= 1 || dice2 <= 6) {
+  if (dice1 === dice2 ) {
+    isWinningDouble = true;
+  } else {
+    isWinningDouble = false;
+  }
+}
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
+if (isWinningDouble) { 
   console.log('Выигрышный дубль!')
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
