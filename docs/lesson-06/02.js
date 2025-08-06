16e3fd307af66e1c105ff6fe5e574b4d8b1f5029
Aliaksei Redzko <aleha.redko@gmail.com>
2025-08-06 23:06:47 +0300

    2 task lesson 6 js ferst try

--- docs/lesson-06/02.js
+++ docs/lesson-06/02.js
@@ -24,4 +24,23 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const numbers = [1, 2, 3, 4, 5]
+
+
+const map = (array, callback) => {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const index = i
+    if (callback(element, index)) {
+      newArray.push(element)
+    }
+  }
+  return newArray
+}
+
+const doubledNumbers = map(numbers, (element, index) => {
+  return element * 2
+})
+
+console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
