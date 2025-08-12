7d24a3ef8d2d4035443061c0a8722b155938f9aa
Aliaksei Redzko <aleha.redko@gmail.com>
2025-08-12 22:42:22 +0300

    get code 7 lesson

--- docs/lesson-07/01.js
+++ docs/lesson-07/01.js
@@ -17,4 +17,16 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+const addIClassSection = document.querySelector('section')
+addIClassSection.classList.add('sectionSix')
+
+const newImg = document.createElement('img')
+newImg.src = WEB_TECH_IMAGES[1]
+newImg.src = WEB_TECH_IMAGES[2]
+
+addIClassSection.appendChild(newImg)
+
+// addImgInSection.document.innerHTML = WEB_TECH_IMAGES
+
+
 
