9fb400cb213100cb9c6b4b4f29c1f0003a9af2c5
Aliaksei Redzko <aleha.redko@gmail.com>
2025-09-03 14:49:51 +0300

    homework task 8 exersize 2 4end try

--- docs/lesson-08/02.js
+++ docs/lesson-08/02.js
@@ -3,19 +3,42 @@
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
  */
 
+// function isNumeric(str) {
+//   if (typeof str !== 'number') {
+//     return false
+//   }
+//   const num = parseFloat(str)
+//   return isNumeric(num)
+// }
+
+/*
+ * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
+ * Если строка является числом, функция должна возвращать true, в противном случае - false.
+ */
+
+// function isNumeric(str) {
+//   if (typeof str !== 'number') {
+//     return false
+//   }
+//   const num = parseFloat(str)
+//   return isNumeric(num)
+// }
+
 function isNumeric(str) {
-if (str === 'string') {
-  return false
-} else{
-  return true
+  if (typeof str !== 'string' || str.trim() === '') {
+    return false;
+  }
+
+  const num = Number(str);
+
+  return isFinite(num);
 }
-}
+
 
 // console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
 // console.log(isNumeric("123abc")) // Ожидаемый результат: false
 // console.log(isNumeric("abc")) // Ожидаемый результат: false
 // console.log(isNumeric(" ")) // Ожидаемый результат: false
-const exemple = isNumeric("abc")
+const exemple = isNumeric("12.3")
 console.log(exemple);
-
